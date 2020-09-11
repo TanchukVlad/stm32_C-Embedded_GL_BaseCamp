ba8da1866a3f6b0fb5758e2f553d94242718bf41
TanchukVlad <tanchuk.vladislav@gmail.com>
2020-09-11 18:55:58 +0300

    Clock - 168MHz

---
+++ inc/clock_168mhz.h
@@ -0,0 +1,10 @@
+#include "tick.h"
+#include <libopencm3/cm3/cortex.h>
+#include <libopencm3/cm3/nvic.h>
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/flash.h>
+#include <libopencm3/stm32/pwr.h>
+#include <stdint.h>
+#include <stddef.h>
+
+void clock_init(void);
