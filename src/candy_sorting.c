369f8eca399e653fa9fa0ae3bbc455f21861bea3
TanchukVlad <60943993+TanchukVlad@users.noreply.github.com>
2020-10-15 14:55:57 +0300

    Update candy_sorting.c

--- src/candy_sorting.c
+++ src/candy_sorting.c
@@ -5,16 +5,16 @@
  *      Servo Motor SG90 - 2 pieces
  *      Color Sensor TCS3200 - 1 pieces
  *      GL Starter Kit with stm32f407VG - Discovery
- * Color is read by Color Sensor which return some frequency for each
+ * Color is read by Color Sensor which returns some frequency for each
  * component(R, G and B).
  * Execution order:
- *      1) Servo 1 directs candy on the Color Sensor;
- *      2) Color Sensor recognize color;
- *      3) Servo 2 set slide on the correct cell depends on color;
- *      4) Servo 1 send candy on the slide.
- * Program starting with calibration. You can set your cells in the correct place.
- * If you was late, you can repeat calibration.
- * During the program, you be able to make pause until you'll press button.
+ *      1) Servo 1 directs the candy on the Color Sensor;
+ *      2) The Color Sensor recognize color;
+ *      3) Servo 2 sets slide on the correct cell depends on color;
+ *      4) Servo 1 sends candy on the slide.
+ * Program starts with calibration. You can set your cells in the correct place.
+ * If you were late, you can repeat calibration.
+ * During the program, you are able to make pause until you press button.
  */
 
 #include "clock_168mhz.h"
